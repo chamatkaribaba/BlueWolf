c8d1da4015a3c8d5c2f85dae680927e67cfc957a
Siddarth <91415157+chamatkaribaba@users.noreply.github.com>
2021-09-26 16:25:31 +0000

    added nqn and some small emoji modifications

--- index.js
+++ index.js
@@ -251,6 +251,43 @@ client.on("message", async (message) => {
 
   
 client.on("message", async (message) => {
+  if (message.author.bot) return;
+  let msg = message.content;
+
+  let emojis = msg.match(/(?<=:)([^:\s]+)(?=:)/g)
+  if (!emojis) return;
+  emojis.forEach(m => {
+    let emoji = client.emojis.cache.find(x => x.name === m)
+    if (!emoji) return;
+    let temp = emoji.toString()
+    if (new RegExp(temp, "g").test(msg)) msg = msg.replace(new RegExp(temp, "g"), emoji.toString())
+    else msg = msg.replace(new RegExp(":" + m + ":", "g"), emoji.toString());
+  })
+
+  if (msg === message.content) return;
+
+  let webhook = await message.channel.fetchWebhooks();
+  let number = randomNumber(1, 2);
+  webhook = webhook.find(x => x.name === "NQN" + number);
+
+  if (!webhook) {
+    webhook = await message.channel.createWebhook(`NQN` + number, {
+      avatar: client.user.displayAvatarURL({ dynamic: true })
+    });
+  }
+
+  await webhook.edit({
+    name: message.member.nickname ? message.member.nickname : message.author.username,
+    avatar: message.author.displayAvatarURL({ dynamic: true })
+  })
+
+  message.delete().catch(err => { })
+  webhook.send(msg).catch(err => { })
+
+  await webhook.edit({
+    name: `NQN` + number,
+    avatar: client.user.displayAvatarURL({ dynamic: true })
+  })
 
   const cchann = db.get(`chatbot_${message.guild.id}`);
   if (cchann === null) return;
@@ -759,3 +796,9 @@ setInterval(async () => {
 run();
 
 client.login("ODQzMDExNzExNzI3OTYwMDc1.YJ9p1g.3MW25uy3xTmvSNMc-1nbsBxcsQ0")
+
+function randomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+} 
